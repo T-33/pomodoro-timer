b2a8e11864e5a5e2abcec14f6a762d125590d62b
Timur Rakhmatullaev <timurrahmatullaev703@gmail.com>
2025-01-29 17:58:30 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿let startButton = document.getElementById("startButton");
+let timeInputField = document.getElementById("timeInputField");
+
+let timeMinutes = 30;
+
+startButton.addEventListener("click", (e) => {
+    timeMinutes = timeInputField.value;
+    console.log(`Time value = ${timeMinutes}`);
+})
